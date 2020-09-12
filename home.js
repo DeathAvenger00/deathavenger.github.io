ec9bafd08ecbb6cf1d4a0e58f7392600c814d739
DeathAvenger00 <54879634+DeathAvenger00@users.noreply.github.com>
2020-09-12 16:13:24 +0530

    Create home.js

---
+++ home.js
@@ -0,0 +1,13 @@
+console.log('hey');
+alert(' I am developing a site ');
+document.getElementById('9').innerHTML = 'Hey soham here';
+document.getElementById('8').innerHTML = 'i am learning java';
+function greeting(){
+   var name= prompt('what is your name');
+    var result= 'Hello '+ name +' how are you';
+    console.log(result);
+    document.getElementById ('6').innerHTML = result;
+}
+greeting ();
+var age = prompt('whats your age');
+document. getElementById ('7').innerHTML= age ;
